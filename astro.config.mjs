a30989b67d28714dfa8a1491cef12d1b940a9b90
Caroline S <cahe0002@stud.ek.dk>
2026-02-11 21:11:39 +0100

    den autosaver ikke

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://tema7-gruppe2.github.io",
+});
